db3c65662a9c6df8ee65bda4c91444fbf7a0ab75
Laurent Navarro <ln@altidev.com>
2015-05-12 07:22:27 +0200

    MAJ

--- appli/static/multizoom.js
+++ appli/static/multizoom.js
@@ -17,7 +17,7 @@
 // Nov 28th, 2012: Version 2.1 w/Multi Zoom, updates - new features and bug fixes
 
 var featuredimagezoomer = { // the two options for Featured Image Zoomer:
-	loadinggif: 'spinningred.gif', // full path or URL to "loading" gif
+	loadinggif: 'static/spinningred.gif', // full path or URL to "loading" gif
 	magnifycursor: 'crosshair' // value for CSS's 'cursor' property when over the zoomable image
 };
 
