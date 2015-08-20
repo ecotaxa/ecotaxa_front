631af397cf263a660256dd817ebc00e5abca2014
Laurent Tosh2 <ln@altidev.com>
2015-08-20 08:29:20 +0200

    filtre sample

--- appli/search/common.py
+++ appli/search/common.py
@@ -10,6 +10,16 @@ def searchusers():
     if len(term)<2:
         return "[]"
     term=R"%"+term+R"%"
-    res = database.GetAll("SELECT id, name FROM users WHERE  name ILIKE %s and active=true order by name limit 1000", (term,),debug=True)
+    res = database.GetAll("SELECT id, name FROM users WHERE  name ILIKE %s and active=true order by name limit 1000", (term,),debug=False)
+    return json.dumps([dict(id=r[0],text=r[1]) for r in res])
+
+
+@app.route("/search/samples")
+def searchsamples():
+    projid=gvg("projid")
+    term="%"+gvg("q")+"%"
+    if projid=="":
+        return "[]"
+    res = database.GetAll("SELECT sampleid, orig_id FROM samples WHERE  projid =%s and orig_id like %s order by orig_id limit 2000", (projid,term),debug=True)
     return json.dumps([dict(id=r[0],text=r[1]) for r in res])
 
