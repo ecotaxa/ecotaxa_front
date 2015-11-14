4755393e87cea965acaad79476f70292b1f66f0a
Laurent Navarro <ln@altidev.com>
2015-11-14 16:44:42 +0100

    MAJ

--- appli/main.py
+++ appli/main.py
@@ -21,23 +21,24 @@ app.register_blueprint(vaultBP)
 
 @app.route('/')
 def index():
-    # flash('Flash Test Info','info')
-    # flash('Flash Test Erreur','error')
-    # flash('Flash Test warning','warning')
-    # #flash('Flash Message')
-    # txt = "<a href=/Task/Create/TaskTest>Task/Create/TaskTest</a><br>"
-    # txt += "<a href=/Task/Create/TaskTaxoSync>Task/Create/TaskTaxoSync</a><br>"
-    # txt += "<a href="+url_for("test1")+">Test 1</a><br>"
-    # txt += "<a href="+url_for("test2")+">Test 2 : Layout seul</a><br>"
-    # txt += "<a href="+url_for("test3")+">Test 3 : Recherche et affichage</a><br>"
-    # txt += "<a href=/Task/Create/TaskImport?p=1>Create task Import</a><br>"
-    # txt += "<a href=/prj/2>Go to project 2</a><br>"
+    txt="""
+	<div class="row" id="homeContent">
+		<div class="col-sm-12">
+"""
     with open('appli/static/home/home.html', 'r') as f:
-        txt= f.read()
-    txt+="""<p align=center>
-    <a href="/explore/" class="btn btn-info btn-lg">Explore All Images</a>
-    <a href="/prj/" class="btn btn-info btn-lg">Select a project</a>
-     </p>"""
+        txt+=f.read()
+    txt+="""
+		</div>
+	</div>
+	<div class="row" id="homeSelectors">
+		<div class="col-sm-6">
+			<a href="/explore/" class="btn btn-primary btn-block btn-lg">Explore all images</a>
+		</div>
+		<div class="col-sm-6">
+			<a href="/prj/" class="btn btn-primary btn-block btn-lg">Select a project</a>
+		</div>		
+	</div>
+"""
 
 
     return PrintInCharte(txt)
@@ -58,30 +59,6 @@ def test1():
     txt += "<br><img src='vault/test.jpg' width=500>"
     return render_template('layout.html',bodycontent=txt)
 
-@app.route('/test2')
-def test2():
-    return render_template('layout.html',bodycontent="Contenu du Haut",bodyleft="<b>Contenu de gauche",bodyright="<h1>Contenu de droite")
-
-@app.route('/test3')
-def test3():
-    left=render_template('search/leftsearchbar.html')
-    return render_template('layout.html',bodycontent="Contenu du Haut",bodyleft=left,bodyright="<h1>Contenu de droite</h1> "
-    +"<img id=IMG10 data-src='/vault/0000/41201_mini.JPG?1' data-zoom-image='0000/41201.JPG?1 ' width=90 height=150 class='lazy'>"
-    # +"<img id=IMG1 src='/vault/0000/41201_mini.JPG?1' data-zoom-image='0000/41201.JPG?1' width=90 height=150 class='lazyx'>"
-    +"""  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <img id=IMG2 src='/vault/0000/41200.JPG?1' data-zoom-image='0000/41200.JPG?1' class='lazy'>
-
-<script>
-    $(document).ready(function() {
-        AddZoom($('#IMG2'))
-    });
-</script>
-    """
-    # +"\n<script> $('img.lazyx').elevateZoom({scrollZoom : true});</script>"
-    # +"\n<script> $('img.lazyx').elevateZoom({ zoomType	: 'lens', lensShape : 'round', lensSize : 200 });</script>"
-
-    # +appli.search.taxo.taxofinal()
-    )
 
 
 @app.route('/testadmin')
