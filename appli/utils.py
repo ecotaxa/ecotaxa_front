71565a4c3c5e7a017304958b418845d59ce0c1bf
grololo06 <laurent.salinas@laposte.net>
2020-09-06 19:19:39 +0200

    Simplify usage of API with a session.

--- appli/utils.py
+++ appli/utils.py
@@ -1,20 +1,25 @@
 #
 # Utility defs not depending on the Flask app.
 #
-from typing import Type, TypeVar, Generic
+from typing import Type, TypeVar, Generic, Union
+
+from flask import Request
+from werkzeug.local import LocalProxy
 
 from appli.api_proxy import BACKEND_URL
-from to_back.ecotaxa_cli_py import ApiClient as _ApiClient, ProjectsApi, UsersApi
+from to_back.ecotaxa_cli_py import ApiClient as _ApiClient, ProjectsApi, UsersApi, ObjectsApi
 
 # Lol, generics in python
-A = TypeVar('A', ProjectsApi, UsersApi)
+A = TypeVar('A', ProjectsApi, UsersApi, ObjectsApi)
 
 
 class ApiClient(Generic[A]):
     """ A client with guaranteed resource released """
 
-    def __init__(self, api_class: Type[A], token: str):
+    def __init__(self, api_class: Type[A], token: Union[str, LocalProxy]):
         api_client = _ApiClient()
+        if isinstance(token, LocalProxy):
+            token = token.cookies.get('session')
         api_client.configuration.access_token = token
         # Note: No trailing / in URL
         api_client.configuration.host = BACKEND_URL
