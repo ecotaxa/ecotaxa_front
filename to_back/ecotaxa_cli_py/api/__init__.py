431d45e939bf79e1f29bf1f5bf3bb6febcb1db82
grololo06 <laurent.salinas@laposte.net>
2020-10-14 06:48:57 +0200

    #435: API version change -> all files changed. Added primitives for objects and parent entities.

--- to_back/ecotaxa_cli_py/api/__init__.py
+++ to_back/ecotaxa_cli_py/api/__init__.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
+from to_back.ecotaxa_cli_py.api.taxonomy_tree_api import TaxonomyTreeApi
 from to_back.ecotaxa_cli_py.api.wip_api import WIPApi
 from to_back.ecotaxa_cli_py.api.acquisitions_api import AcquisitionsApi
 from to_back.ecotaxa_cli_py.api.authentification_api import AuthentificationApi
 from to_back.ecotaxa_cli_py.api.misc_api import MiscApi
+from to_back.ecotaxa_cli_py.api.object_api import ObjectApi
 from to_back.ecotaxa_cli_py.api.objects_api import ObjectsApi
 from to_back.ecotaxa_cli_py.api.processes_api import ProcessesApi
 from to_back.ecotaxa_cli_py.api.projects_api import ProjectsApi
