4544706c1a0c6ca1eb6a251fd8c3f5d187b756eb
grololo06 <laurent.salinas@laposte.net>
2020-11-17 11:08:59 +0100

    #523, #435: Use API _only_ for merge.

--- appli/project/merge.py
+++ appli/project/merge.py
@@ -3,30 +3,34 @@ from typing import List
 from flask import g, flash, request
 from flask_security import login_required
 
-from appli import app, PrintInCharte, database, gvg, XSSEscape, FormatError
+from appli import app, PrintInCharte, gvg, XSSEscape, FormatError
 ######################################################################################################################
 from appli.utils import ApiClient
-from to_back.ecotaxa_cli_py import ProjectsApi, ProjectModel, MergeRsp
+from to_back.ecotaxa_cli_py import ProjectsApi, ProjectModel, MergeRsp, ApiException
 
 
 @app.route('/prj/merge/<int:PrjId>', methods=['GET', 'POST'])
 @login_required
 def PrjMerge(PrjId):
-    Prj = database.Projects.query.filter_by(projid=PrjId).first()
-    if Prj is None:
-        flash("Project doesn't exists", 'error')
-        return PrintInCharte("<a href=/prj/>Select another project</a>")
-    if not Prj.CheckRight(2):  # Level 0 = Read, 1 = Annotate, 2 = Admin
-        flash('You cannot edit settings for this project', 'error')
-        return PrintInCharte("<a href=/prj/>Select another project</a>")
-    g.headcenter = "<h4><a href='/prj/{0}'>{1}</a></h4>".format(Prj.projid, XSSEscape(Prj.title))
+    # Security & sanity checks
+    with ApiClient(ProjectsApi, request) as api:
+        try:
+            target_proj: ProjectModel = api.project_query_projects_project_id_get(PrjId, for_managing=True)
+        except ApiException as ae:
+            if ae.status == 404:
+                return "Project doesn't exist"
+            elif ae.status == 403:
+                flash('You cannot merge this project', 'error')
+                return PrintInCharte("<a href=/prj/>Select another project</a>")
+
+    g.headcenter = "<h4><a href='/prj/{0}'>{1}</a></h4>".format(target_proj.projid, XSSEscape(target_proj.title))
     txt = "<h3>Project Merge / Fusion </h3>"
 
     if not gvg('src'):
         # No submit -> preliminary page display
         txt += """<ul><li>You are allowed to merge projects that you are allowed to manage
 <li>User privileges from both projects will be added
-<li>This tool allow to merge two projects in a single projet (called Current project). The added project will then be automatically deleted. If object data are not consistent between both projects :
+<li>This tool allows to merge two projects in a single projet (called Current project). The added project will then be automatically deleted. If object data are not consistent between both projects :
 <ul><li>New data fields are added to the Current project
     <li>The resulting project will thus contain partially documented datafields.
 </ul><li>Note : Next screen will indicate compatibility issues (if exists) and allow you to Confirm the merging operation.
@@ -43,33 +47,43 @@ def PrjMerge(PrjId):
             <th width=100>% Validated</th><th width=100>% Classified</th></tr>"""
         for r in rsp:
             # Don't merge into self :)
-            if r.projid == Prj.projid:
+            if r.projid == target_proj.projid:
                 continue
+            if r.objcount is None:
+                r.objcount = 0
+            if r.pctclassified is None:
+                r.pctclassified = 0
+            if r.pctvalidated is None:
+                r.pctvalidated = 0
             txt += """<tr><td><a class="btn btn-primary" href='/prj/merge/{activeproject}?src={_projid}'>Select</a> {_projid}</td>
             <td>{_title}</td>
             <td>{_status}</td>
             <td>{_objcount:0.0f}</td>
             <td>{_pctvalidated:0.2f}</td>
             <td>{_pctclassified:0.2f}</td>
-            </tr>""".format(activeproject=Prj.projid, **r.__dict__)
+            </tr>""".format(activeproject=target_proj.projid, **r.__dict__)
         txt += "</table>"
         return PrintInCharte(txt)
 
-    PrjSrc = database.Projects.query.filter_by(projid=int(gvg('src'))).first()
-    if PrjSrc is None:
-        flash("Source project doesn't exists", 'error')
-        return PrintInCharte("<a href=/prj/>Select another project</a>")
-    if not PrjSrc.CheckRight(2):  # Level 0 = Read, 1 = Annotate, 2 = Admin
-        flash('You cannot merge for this project', 'error')
-        return PrintInCharte("<a href=/prj/>Select another project</a>")
+    src_prj_id = int(gvg('src'))
+    with ApiClient(ProjectsApi, request) as api:
+        try:
+            source_proj: ProjectModel = api.project_query_projects_project_id_get(src_prj_id, for_managing=True)
+        except ApiException as ae:
+            if ae.status == 404:
+                return "Source project doesn't exist"
+            elif ae.status == 403:
+                flash('You cannot merge from this project', 'error')
+                return PrintInCharte("<a href=/prj/>Select another project</a>")
+
     txt += """<h4>Source Project : {0} - {1} (This project will be destroyed)</h4>
-            """.format(PrjSrc.projid, XSSEscape(PrjSrc.title))
+            """.format(source_proj.projid, XSSEscape(source_proj.title))
 
     if not gvg('merge'):  # Ici la src à été choisie et vérifiée
         # Validate the merge
         with ApiClient(ProjectsApi, request.cookies.get('session')) as api:
-            rsp: MergeRsp = api.project_merge_projects_project_id_merge_post(project_id=Prj.projid,
-                                                                             source_project_id=PrjSrc.projid,
+            rsp: MergeRsp = api.project_merge_projects_project_id_merge_post(project_id=target_proj.projid,
+                                                                             source_project_id=source_proj.projid,
                                                                              dry_run=True)
 
         for an_error in rsp.errors:
@@ -81,7 +95,7 @@ def PrjMerge(PrjId):
             Will be destroyed, its content will be transfered in the target project.<br>
             This operation is irreversible</p>
             <br><a class='btn btn-lg btn-warning' href='/prj/merge/{0}?src={1}&merge=Y'>Start Project Fusion</a>        
-            """, Prj.projid, PrjSrc.projid, XSSEscape(PrjSrc.title), DoNotEscape=True)
+            """, target_proj.projid, source_proj.projid, XSSEscape(source_proj.title), DoNotEscape=True)
             return PrintInCharte(txt)
         else:
             return PrintInCharte("Hit \"Back\" on the navigator to pick another source project.")
@@ -89,10 +103,10 @@ def PrjMerge(PrjId):
     if gvg('merge') == 'Y':
         # Do the real merge
         with ApiClient(ProjectsApi, request.cookies.get('session')) as api:
-            rsp: MergeRsp = api.project_merge_projects_project_id_merge_post(project_id=Prj.projid,
-                                                                             source_project_id=PrjSrc.projid,
+            rsp: MergeRsp = api.project_merge_projects_project_id_merge_post(project_id=target_proj.projid,
+                                                                             source_project_id=source_proj.projid,
                                                                              dry_run=False)
 
         txt += "<div class='alert alert-success' role='alert'>Fusion Done successfully</div>"
-        txt += "<br><a class='btn btn-lg btn-primary' href='/prj/%s'>Back to target project</a>" % Prj.projid
+        txt += "<br><a class='btn btn-lg btn-primary' href='/prj/%s'>Back to target project</a>" % target_proj.projid
         return PrintInCharte(txt)
