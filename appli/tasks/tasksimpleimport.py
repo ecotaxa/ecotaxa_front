ad183ba3ecfcf64dcd5f22c070027a5d0f66c836
grololo06 <laurent.salinas@laposte.net>
2021-02-03 09:15:47 +0100

    #436: Use API call during import, no DB direct access anymore.

--- appli/tasks/tasksimpleimport.py
+++ appli/tasks/tasksimpleimport.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 import time
-from typing import Dict
+from typing import Dict, List
 
 from flask import render_template
 from flask_login import current_user
 
-from appli import database, PrintInCharte, gvg
+from appli import PrintInCharte, gvg
 from appli.tasks.importcommon import *
 from appli.tasks.taskmanager import AsyncTask
 from appli.utils import ApiClient
-from to_back.ecotaxa_cli_py import SimpleImportReq, SimpleImportRsp, ProjectsApi
+from to_back.ecotaxa_cli_py import SimpleImportReq, SimpleImportRsp, ProjectsApi, ProjectModel, ApiException, \
+    UsersApi, UserModel, TaxonomyTreeApi, TaxonModel
 
 
+# noinspection DuplicatedCode
 class TaskSimpleImport(AsyncTask):
     """
         Simple Import, just GUI here, bulk of job subcontracted to back-end.
@@ -41,10 +43,13 @@ class TaskSimpleImport(AsyncTask):
 
     def _CreateDialogStep0(self):
         """ In UI/flask, task.taskstep = 0 AKA initial dialog """
-        prj_id = gvg("p")
-        prj = database.Projects.query.filter_by(projid=prj_id).first()
-        if not prj.CheckRight(1):
-            return PrintInCharte("ACCESS DENIED for this project")
+        prj_id = int(gvg("p"))
+        with ApiClient(ProjectsApi, request) as api:
+            try:
+                _target_prj: ProjectModel = api.project_query_projects_project_id_get(prj_id, for_managing=False)
+            except ApiException as ae:
+                if ae.status == 403:
+                    return PrintInCharte("ACCESS DENIED for this project")
         self.param.ProjectId = prj_id
         preset = self.SimpleImportPreset.setdefault(current_user.id, {})
 
@@ -55,7 +60,7 @@ class TaskSimpleImport(AsyncTask):
             # Form Submit -> check and store values to use
             errors = []
             # Validate/store import file parameter
-            FileToSave, FileToSaveFileName = get_file_from_form(self, errors)
+            file_to_save, file_to_save_file_name = get_file_from_form(self, errors)
             # Use backend to validate the form
             values = {}
             self.param.values = values
@@ -68,26 +73,30 @@ class TaskSimpleImport(AsyncTask):
                 if values[fld] == "":
                     values[fld] = None
                 preset[fld] = values[fld]
-            with ApiClient(ProjectsApi, self.cookie) as api:
+            with ApiClient(ProjectsApi, request) as api:
                 rsp: SimpleImportRsp = api.simple_import_simple_import_project_id_post(self.param.ProjectId, req)
             errors.extend(rsp.errors)
             # Check for errors. If any, stay in current state.
             if not flash_any_error(errors):
                 # start Step1, in a subprocess as we're in UI process, reusing log files
-                return self.StartTask(self.param, step=1, FileToSave=FileToSave, FileToSaveFileName=FileToSaveFileName)
+                return self.StartTask(self.param, step=1,
+                                      FileToSave=file_to_save,
+                                      FileToSaveFileName=file_to_save_file_name)
 
         # Display the form, enrich it first
         # Example: preset = {"imgdate": "20150327", "imgtime": "1447",
         #                    "latitude": "-12.06398", "longitude": "-135.05325","depthmin": "50",
         #                    "depthmax": "70","userlb":8, "status":'V', "taxolb":25827}
         if 'userlb' in preset and preset['userlb'] is not None:
-            usr = database.users.query.filter_by(id=preset['userlb']).first()
-            if usr:
-                preset["annot_name"] = usr.name
+            with ApiClient(UsersApi, request) as api:
+                user: UserModel = api.get_user_users_user_id_get(user_id=int(preset['userlb']))
+            if user:
+                preset["annot_name"] = user.name
         if 'taxolb' in preset and preset['taxolb'] is not None:
-            taxo = database.Taxonomy.query.filter_by(id=preset['taxolb']).first()
-            if taxo:
-                preset["taxo_name"] = taxo.name
+            with ApiClient(TaxonomyTreeApi, request) as api:
+                nodes: List[TaxonModel] = api.query_taxa_set_taxon_set_query_get(ids=preset['taxolb'])
+            if nodes:
+                preset["taxo_name"] = nodes[0].name
         return PrintInCharte(render_template("project/simpleimport.html", preset=preset))
 
     # noinspection PyPep8Naming
