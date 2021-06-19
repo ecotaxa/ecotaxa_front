4557b741ef9d5e3df34bf015fad6a9959d4a579a
grololo06 <laurent.salinas@laposte.net>
2021-06-19 09:34:12 +0200

    Better config for switching b/w various envs types.

--- proto/ecotaxa-cli/vue.config.js
+++ proto/ecotaxa-cli/vue.config.js
@@ -12,11 +12,16 @@ module.exports = {
     proxy: {
       "/api": {
         target: "https://ecotaxa.obs-vlfr.fr/",
+        // Uncomment below for using local python server which does proxying on /api to local docker
+        //target: "http://localhost:5001/",
+        // Uncomment below for using local docker
+        //target: "http://localhost:8000/",
         ws: true,
         changeOrigin: true,
         onProxyReq: (proxyReq, req) => {
-          // Remove session cookies which are the session b/w client and dev web server
-          if ("cookie" in req.headers) {
+          // Remove session cookies which are the session b/w client and dev web server, thus useless to prod' site
+          // TODO: A real login via API in such case
+          if ("cookie" in req.headers && proxyReq.host !== "localhost") {
             proxyReq.removeHeader("cookie");
           }
         },
