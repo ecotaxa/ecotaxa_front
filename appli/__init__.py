4f4fae318d248ab302ff52dc3151ce125225d711
grololo06 <laurent.salinas@laposte.net>
2021-12-01 09:54:02 +0100

    #747: Copy or move what's needed from EcoTaxa to EcoPart. No more access to EcoTaxa 'appli' module from debugger.

--- appli/__init__.py
+++ appli/__init__.py
@@ -262,12 +262,6 @@ def ComputeLimitForImage(imgwidth, imgheight, LimitWidth, LimitHeight):
     return width, height
 
 
-def GetAppManagerMailto():
-    # Left here for EcoPart
-    if 'APPMANAGER_EMAIL' in app.config and 'APPMANAGER_NAME' in app.config:
-        return "<a href='mailto:{APPMANAGER_EMAIL}'>{APPMANAGER_NAME} ({APPMANAGER_EMAIL})</a>".format(**app.config)
-    return ""
-
 
 def CalcAstralDayTime(Date, Time, Latitude, Longitude):
     """
