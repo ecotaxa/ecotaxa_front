98729c112eb07eb1489a6b97f92b8cc2337a5c72
grololo06 <PouGH98!>
2022-02-14 10:27:12 +0100

    #765: Get administrators list using an API call, and depending on auth status.

--- appli/admin/admin_blueprint.py
+++ appli/admin/admin_blueprint.py
@@ -1,6 +1,11 @@
+from typing import List
+
 from flask import Blueprint, render_template
 from flask_admin import helpers
 from flask_admin.helpers import get_url
+from flask_login import current_user
+
+from to_back.ecotaxa_cli_py import UsersApi, MinUserModel
 
 adminBlueprint = Blueprint(name='admin_custom',
                            import_name=__name__,
@@ -24,17 +29,20 @@ def render_in_admin_blueprint(template, **kwargs):
     return render_template(template, **render_ctx)
 
 
-def GetAdminList():
-    from appli.database import GetAll
-    LstUsers = GetAll("""select name||'('||email||')' nom from users u
-                        join users_roles r on u.id=r.user_id where r.role_id=1""")
-    return ", ".join([r[0] for r in LstUsers])
-
-
 @adminBlueprint.route('/')
 def our_admin_root():
+    if current_user.is_authenticated:
+        # With a connected user, return administrators
+        with ApiClient(UsersApi, request) as api:
+            admin_users: List[MinUserModel] = api.get_admin_users()
+    else:
+        # With an anonymous user, return user administrators (for account issues)
+        with ApiClient(UsersApi, request) as api:
+            admin_users: List[MinUserModel] = api.get_users_admins()
+
+    admins = ", ".join(["%s(%s)" % (r.name, r.email) for r in admin_users])
     return render_in_admin_blueprint('admin2/index_with_others.html',
-                                     GetAdminList=GetAdminList)
+                                     admin_users=admins)
 
 
 # Import extra routes
