f35d1148d7a265f52c3c1b0fea57d401ce55ceb9
grololo06 <laurent.salinas@laposte.net>
2022-05-04 06:11:34 +0200

    Tag latest docker as such.

--- docker/build_prod.sh
+++ docker/build_prod.sh
@@ -11,3 +11,4 @@ docker build $NO_CACHE -t ecotaxa/ecotaxa_front -f prod_image/Dockerfile .
 # Push to docker hub
 docker tag ecotaxa/ecotaxa_front:latest ecotaxa/ecotaxa_front:$VERSION
 docker push ecotaxa/ecotaxa_front:$VERSION
+docker push ecotaxa/ecotaxa_front:latest
