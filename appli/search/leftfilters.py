6977a21c7725f13db5f64b2c275e47b2b2d483f3
bea777515 <beatrice.caraveo@imev-mer.fr>
2022-05-23 15:14:47 +0200

    #815: fix

--- appli/search/leftfilters.py
+++ appli/search/leftfilters.py
@@ -5,15 +5,21 @@ from flask import render_template, g, request, json
 
 from appli import app, gvg
 from appli.utils import ApiClient, get_all_visible_projects
-from to_back.ecotaxa_cli_py import SamplesApi, SampleModel, ProjectsApi, ProjectModel, ApiException
+from to_back.ecotaxa_cli_py import (
+    SamplesApi,
+    SampleModel,
+    ProjectsApi,
+    ProjectModel,
+    ApiException,
+)
 
 
-@app.route('/search/mappopup/')
+@app.route("/search/mappopup/")
 def search_mappopup():
     g.filtertxt = ""
-    if gvg('projid'):
-        g.Projid = gvg('projid')
-        projids = [int(x) for x in gvg("projid").split(',')]
+    if gvg("projid"):
+        g.Projid = gvg("projid")
+        projids = [int(x) for x in gvg("projid").split(",")]
         titles = []
         with ApiClient(ProjectsApi, request) as api:
             for a_proj in projids:
@@ -35,23 +41,24 @@ def search_mappopup():
     #         g.taxochild = gvg('taxochild')
     #         g.filtertxt += "(with child)"
 
-    return render_template('search/mappopup.html')
+    return render_template("search/mappopup.html")
 
 
-@app.route('/search/mappopup/samples/')
+@app.route("/search/mappopup/samples/")
 def search_mappopup_samples():
     """
-        AJAX call to return data for drawing samples points in the global map.
+    AJAX call to return data for drawing samples points in the global map.
     """
     app.logger.info("Search mappopup req: %s", request.args)
-    if gvg('projid'):
-        projids = [int(x) for x in gvg("projid").split(',')]
+    if gvg("projid"):
+        projids = [int(x) for x in gvg("projid").split(",")]
     else:
         projids = [a_proj.projid for a_proj in get_all_visible_projects()]
     str_project_ids = "+".join([str(p) for p in projids])
     with ApiClient(SamplesApi, request) as api:
-        samples: List[SampleModel] = api.samples_search(project_ids=str_project_ids,
-                                                        id_pattern='%')
+        samples: List[SampleModel] = api.samples_search(
+            project_ids=str_project_ids, id_pattern="%"
+        )
 
     # TODO: Put into API one day
     #     if gvg('taxoid'):
@@ -70,14 +77,21 @@ def search_mappopup_samples():
 
     ores = []
     for sam in samples:
-        ores.append({'id': sam.projid, 'lat': sam.latitude, 'long': sam.longitude})
+        ores.append(
+            {
+                "id": sam.sampleid,
+                "projid": sam.projid,
+                "lat": sam.latitude,
+                "long": sam.longitude,
+            }
+        )
     return json.dumps(ores)
 
 
-@app.route('/search/mappopup/getsamplepopover/<int:sampleid>')
+@app.route("/search/mappopup/getsamplepopover/<int:sampleid>")
 def search_mappopup_samplepopover(sampleid: int):
     """
-        AJAX call for getting a sample information.
+    AJAX call for getting a sample information.
     """
     try:
         with ApiClient(SamplesApi, request) as sapi:
@@ -90,11 +104,16 @@ def search_mappopup_samplepopover(sampleid: int):
     Original ID : {orig_id}<br>
     Project : {title} ({projid})<br>
     Lat/Lon : {latitude}/{longitude}
-    """.format(sampleid=sample.sampleid, orig_id=sample.orig_id,
-               title=project.title, projid=project.projid,
-               latitude=round(sample.latitude, 4), longitude=round(sample.longitude, 4))
+    """.format(
+        sampleid=sample.sampleid,
+        orig_id=sample.orig_id,
+        title=project.title,
+        projid=project.projid,
+        latitude=round(sample.latitude, 4),
+        longitude=round(sample.longitude, 4),
+    )
     return txt
 
 
 def getcommonfilters(data) -> str:
-    return render_template('search/commonfilters.html', data=data)
+    return render_template("search/commonfilters.html", data=data)
