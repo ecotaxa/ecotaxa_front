78e44e74ff5f620de1198a5c44906ba9cabbbfdc
bea777515 <beatrice.caraveo@imev-mer.fr>
2022-05-30 10:42:19 +0200

    #820: fix

--- appli/admin/views/users.py
+++ appli/admin/views/users.py
@@ -3,8 +3,15 @@ from typing import List
 from flask import request, flash
 from flask_admin.helpers import get_form_data
 from flask_login import current_user
-from wtforms import PasswordField, TextAreaField, SelectField, ValidationError, EmailField, SelectMultipleField, \
-    HiddenField
+from wtforms import (
+    PasswordField,
+    TextAreaField,
+    SelectField,
+    ValidationError,
+    EmailField,
+    SelectMultipleField,
+    HiddenField,
+)
 
 import appli.constants
 from appli import constants
@@ -24,53 +31,63 @@ class UsersView(APIModelView):
     form_base_class = SecureStrippingBaseForm
 
     # Form tuning - List View
-    column_exclude_list = ('usercreationreason', 'last_used_projects', 'can_do', 'password')
-    column_searchable_list = ('email', 'name')
+    column_exclude_list = (
+        "usercreationreason",
+        "last_used_projects",
+        "can_do",
+        "password",
+    )
+    column_searchable_list = ("email", "name")
     # Form tuning - Detail view
-    form_excluded_columns = ('last_used_projects')
+    form_excluded_columns = "last_used_projects"
     form_overrides = {
-        'id': HiddenField,
-        'email': EmailField,
-        'password': PasswordField,
-        'usercreationreason': TextAreaField,
-        'country': SelectField,
-        'can_do': SelectMultipleField
+        "id": HiddenField,
+        "email": EmailField,
+        "password": PasswordField,
+        "usercreationreason": TextAreaField,
+        "country": SelectField,
+        "can_do": SelectMultipleField,
     }
     # Additions
-    mandatory_columns = ('email', 'name', 'usercreationdate')
+    mandatory_columns = ("email", "name", "country", "usercreationdate")
 
     def __init__(self):
         super(UsersView, self).__init__(model=UserModelWithRights, name="Users")
 
     def update_model(self, form, model):
-        """ Called on "save" of existing model """
+        """Called on "save" of existing model"""
         # No way to modify data from the POST, other than referencing this private member
         # noinspection PyProtectedMember
         form_fields = form._fields
         # Change password _only_ if it's been filled, with confirmation, as browsers tend to remember password
-        if not (form_fields['password'].data and form_fields['password_confirm'].data):
-            form_fields['password'].data = None
+        if not (form_fields["password"].data and form_fields["password_confirm"].data):
+            form_fields["password"].data = None
         # The forms stores a datetime, the API needs a str
-        form_fields['usercreationdate'].data = form_fields['usercreationdate'].data.isoformat() + " 00:00:00"
+        form_fields["usercreationdate"].data = (
+            form_fields["usercreationdate"].data.isoformat() + " 00:00:00"
+        )
         try:
             with ApiClient(UsersApi, request) as api:
-                ret = api.update_user(user_id=form.data['id'],
-                                      user_model_with_rights=form.data)
+                ret = api.update_user(
+                    user_id=form.data["id"], user_model_with_rights=form.data
+                )
         except ApiException as ae:
-            flash(str(ae), 'error')
+            flash(str(ae), "error")
 
     def create_model(self, form):
-        """ Called on "save" of new model """
+        """Called on "save" of new model"""
         # noinspection PyProtectedMember
         form_fields = form._fields
         # The forms stores a datetime, the API needs a str
-        form_fields['usercreationdate'].data = form_fields['usercreationdate'].data.isoformat() + " 00:00:00"
-        form_fields['id'].data = -1
+        form_fields["usercreationdate"].data = (
+            form_fields["usercreationdate"].data.isoformat() + " 00:00:00"
+        )
+        form_fields["id"].data = -1
         try:
             with ApiClient(UsersApi, request) as api:
                 ret = api.create_user(user_model_with_rights=form.data)
         except ApiException as ae:
-            flash(str(ae), 'error')
+            flash(str(ae), "error")
 
     @staticmethod
     def check_password_confirm(form, field):
@@ -87,9 +104,11 @@ class UsersView(APIModelView):
         form = self._edit_form_class(get_form_data(), obj=obj)
         countries = [(nm, nm) for nm in get_country_names(request)]
         countries.sort()
-        form.country.choices = [('', '')] + countries
+        form.country.choices = [("", "")] + countries
         form.can_do.label.text = "Roles"
-        form.can_do.choices = [(str(num), lbl) for num, lbl in constants.API_GLOBAL_ROLES.items()]
+        form.can_do.choices = [
+            (str(num), lbl) for num, lbl in constants.API_GLOBAL_ROLES.items()
+        ]
         return form
 
     def create_form(self, obj=None):
@@ -98,15 +117,16 @@ class UsersView(APIModelView):
     def is_accessible(self):
         return current_user.has_role(appli.constants.AdministratorLabel)
 
-    edit_template = 'admin2/users_edit.html'
-    create_template = 'admin2/users_create.html'
+    edit_template = "admin2/users_edit.html"
+    create_template = "admin2/users_create.html"
 
     # Added methods to compensate the lack of DB behind
 
     def scaffold_form(self):
         form_class = super(UsersView, self).scaffold_form()
-        form_class.password_confirm = PasswordField('Password Confirmation',
-                                                    validators=[self.check_password_confirm])
+        form_class.password_confirm = PasswordField(
+            "Password Confirmation", validators=[self.check_password_confirm]
+        )
         return form_class
 
     def init_search(self):
@@ -118,26 +138,25 @@ class UsersView(APIModelView):
     def get_filters(self) -> None:
         return None
 
-    def get_list(self, page, sort_field, sort_desc, search, filters,
-                 page_size=None):
+    def get_list(self, page, sort_field, sort_desc, search, filters, page_size=None):
         """
-            Return a paginated and sorted list of models from the data source.
+        Return a paginated and sorted list of models from the data source.
 
-            :param page:
-                Page number, 0 based. Can be set to None if it is first page.
-            :param sort_field:
-                Sort column name or None.
-            :param sort_desc:
-                If set to True, sorting is in descending order.
-            :param search:
-                Search query
-            :param filters:
-                List of filter tuples. First value in a tuple is a search
-                index, second value is a search value.
-            :param page_size:
-                Number of results. Defaults to ModelView's page_size. Can be
-                overriden to change the page_size limit. Removing the page_size
-                limit requires setting page_size to 0 or False.
+        :param page:
+            Page number, 0 based. Can be set to None if it is first page.
+        :param sort_field:
+            Sort column name or None.
+        :param sort_desc:
+            If set to True, sorting is in descending order.
+        :param search:
+            Search query
+        :param filters:
+            List of filter tuples. First value in a tuple is a search
+            index, second value is a search value.
+        :param page_size:
+            Number of results. Defaults to ModelView's page_size. Can be
+            overriden to change the page_size limit. Removing the page_size
+            limit requires setting page_size to 0 or False.
         """
         # We have no server-side filtering or pagination, so read all and do in-memory operations
         with ApiClient(UsersApi, request) as api:
