49ed739483f7993569cb7ad117618ec75e7273fc
bea777515 <beatrice.caraveo@imev-mer.fr>
2022-06-08 13:51:31 +0200

    #734 #794 - missing back to project in header

--- appli/project/editproject.py
+++ appli/project/editproject.py
@@ -36,7 +36,9 @@ def PrjEdit(PrjId, privs_only=False):
                 flash("You cannot edit settings for this project", "error")
                 return PrintInCharte("<a href=/prj/>Select another project</a>")
 
-    # g.headcenter = "<h4><a href='/prj/{0}'>{1}</a></h4>".format(target_proj.projid, XSSEscape(target_proj.title))
+    g.headcenter = "<h4><a href='/prj/{0}'>{1}</a></h4>".format(
+        target_proj.projid, XSSEscape(target_proj.title)
+    )
     # g.privs_only = privs_only
     # Reconstitute members list with privs
 
