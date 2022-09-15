51b91413f7739988660b21c4f3bbe16ce9a2fdfc
bea777515 <beatrice.caraveo@imev-mer.fr>
2022-09-15 18:19:18 +0200

    remove vue version - will not be used anymore

--- appli/project/index.py
+++ appli/project/index.py
@@ -7,45 +7,52 @@ from flask_security import login_required
 
 from appli import app, PrintInCharte, gvg, gvgm
 from appli.back_config import get_app_manager_mail
-from appli.project.__init__ import connectPythonToPrime
+
 ######################################################################################################################
-from appli.project.index_vue import vue_projects_index
 from appli.project.main import _manager_mail
 from appli.utils import ApiClient, BuildManagersMail
 from to_back.ecotaxa_cli_py.api import ProjectsApi, TaxonomyTreeApi
-from to_back.ecotaxa_cli_py.models import ProjectModel, UserModelWithRights, TaxonomyTreeStatus
+from to_back.ecotaxa_cli_py.models import (
+    ProjectModel,
+    UserModelWithRights,
+    TaxonomyTreeStatus,
+)
 
 
 # noinspection PyPep8Naming
-@app.route('/prj/')
+@app.route("/prj/")
 @login_required
 def indexProjects(Others=False):
-    filt_title = gvg('filt_title', session.get('prjfilt_title', ''))
-    session['prjfilt_title'] = filt_title
+    filt_title = gvg("filt_title", session.get("prjfilt_title", ""))
+    session["prjfilt_title"] = filt_title
 
-    if 'filt_title' in request.args:
+    if "filt_title" in request.args:
         # It was a form submit, but _only_ 'filt_title' is _always_ posted,
         # both the checkbox and the select2 are not transmitted if not filled
-        filt_instrum = gvgm('filt_instrum')  # Get instrument from posted
-        session['prjfilt_instrum'] = "|".join(filt_instrum)
-        filt_subset = gvg('filt_subset', '')  # Get subset filter from posted
-        session['prjfilt_subset'] = filt_subset
+        filt_instrum = gvgm("filt_instrum")  # Get instrument from posted
+        session["prjfilt_instrum"] = "|".join(filt_instrum)
+        filt_subset = gvg("filt_subset", "")  # Get subset filter from posted
+        session["prjfilt_subset"] = filt_subset
     else:
-        sess_filt_instrum = session.get('prjfilt_instrum', '')
+        sess_filt_instrum = session.get("prjfilt_instrum", "")
         if sess_filt_instrum:
             filt_instrum = sess_filt_instrum.split("|")
         else:
             filt_instrum = []
-        filt_subset = session.get('prjfilt_subset', '')
+        filt_subset = session.get("prjfilt_subset", "")
 
     prjs: List[ProjectModel] = []
-    qry_filt_instrum = [''] if len(filt_instrum) == 0 else filt_instrum
+    qry_filt_instrum = [""] if len(filt_instrum) == 0 else filt_instrum
     for an_instrument in qry_filt_instrum:
         with ApiClient(ProjectsApi, request) as apiProj:
-            prjs.extend(apiProj.search_projects(also_others=Others,
-                                                title_filter=filt_title,
-                                                instrument_filter=an_instrument,
-                                                filter_subset=(filt_subset == 'Y')))
+            prjs.extend(
+                apiProj.search_projects(
+                    also_others=Others,
+                    title_filter=filt_title,
+                    instrument_filter=an_instrument,
+                    filter_subset=(filt_subset == "Y"),
+                )
+            )
     # Sort for consistency
     prjs.sort(key=lambda prj: prj.title.strip().lower())
 
@@ -62,45 +69,57 @@ def indexProjects(Others=False):
         status: TaxonomyTreeStatus = apiTaxo.taxa_tree_status()
         try:
             last_refresh = datetime.datetime.strptime(
-                status.last_refresh, '%Y-%m-%dT%H:%M:%S')
+                status.last_refresh, "%Y-%m-%dT%H:%M:%S"
+            )
         except (ValueError, TypeError):
             last_refresh = None
     if last_refresh is None or (datetime.datetime.now() - last_refresh).days > 7:
-        fashtxt = "Taxonomy synchronization and Ecotaxa version check wasn’t done during the last 7 days, " \
-                  "Ask application administrator to do it."  # +str(PDT.lastserverversioncheck_datetime)
+        fashtxt = (
+            "Taxonomy synchronization and Ecotaxa version check wasn’t done during the last 7 days, "
+            "Ask application administrator to do it."
+        )  # +str(PDT.lastserverversioncheck_datetime)
         fashtxt += "  <a href='/taxo/browse/' class='btn btn-primary btn-xs'>Synchronize to check Ecotaxa version</a>"
-        flash(Markup(fashtxt), 'warning')
+        flash(Markup(fashtxt), "warning")
 
     # Construct a mailto: link, in case the instrument is not found
-    mailto_instrument = BuildManagersMail(link_text="Not in the list",
-                                          subject="Request for adding an instrument to EcoTaxa",
-                                          body="""**Information for creation**
-                                          
+    mailto_instrument = BuildManagersMail(
+        link_text="Not in the list",
+        subject="Request for adding an instrument to EcoTaxa",
+        body="""**Information for creation**
+
 Instrument name:
 URL of the description in the BODC L22 vocabulary http://vocab.nerc.ac.uk/collection/L22/current/ :
 
 **Reason for creation**
 Explain how widely the instrument is distributed and why it should be added to the standard list.
-""")
+""",
+    )
     # App manager mail for granting right to create
-    mailto_create_right = get_app_manager_mail(request, "EcoTaxa : Please provide me the Project creation right")
-    if connectPythonToPrime:
-        return vue_projects_index(PrjList=prjs, user=user, CanCreate=CanCreate,
-                                  filt_title=filt_title, filt_subset=filt_subset, filt_instrum=filt_instrum,
-                                  Others=Others)
-    else:  # "historic" code
-        return PrintInCharte(
-            render_template('project/list.html', PrjList=prjs, CanCreate=CanCreate,
-                            filt_title=filt_title, filt_subset=filt_subset, filt_instrum=filt_instrum,
-                            Others=Others, isadmin=2 in user.can_do, mailto_instrument=mailto_instrument,
-                            mailto_create_right=mailto_create_right,
-                            _manager_mail=_manager_mail))
+    mailto_create_right = get_app_manager_mail(
+        request, "EcoTaxa : Please provide me the Project creation right"
+    )
+    # "historic" code only removed connectPythontoPrime
+    return PrintInCharte(
+        render_template(
+            "project/list.html",
+            PrjList=prjs,
+            CanCreate=CanCreate,
+            filt_title=filt_title,
+            filt_subset=filt_subset,
+            filt_instrum=filt_instrum,
+            Others=Others,
+            isadmin=2 in user.can_do,
+            mailto_instrument=mailto_instrument,
+            mailto_create_right=mailto_create_right,
+            _manager_mail=_manager_mail,
+        )
+    )
 
 
 ######################################################################################################################
 
 
-@app.route('/prjothers/')
+@app.route("/prjothers/")
 @login_required
 def ProjectsOthers():
     return indexProjects(Others=True)
