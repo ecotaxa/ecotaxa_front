1625f18a8c8d068594813f50fed115bca9f1b663
bea777515 <beatrice.caraveo@imev-mer.fr>
2022-10-14 11:59:03 +0200

    experimental

--- appli/gui/project/project_stats.py
+++ appli/gui/project/project_stats.py
@@ -1,6 +1,5 @@
-from flask import request, render_template, flash
+from flask import request, render_template
 from appli.utils import ApiClient, DecodeEqualList
-from appli import gvg
 from to_back.ecotaxa_cli_py.models import ProjectSetColumnStatsModel
 from to_back.ecotaxa_cli_py.api import ProjectsApi
 
@@ -13,8 +12,13 @@ def prj_stats(prjid: int, partial: bool, params: dict) -> str:
         # Hidden FORM summarizing the previous steps choices
     limit = params["limit"]
     names_for_stats = ""
-    # Stats for about
-    return render_template("v2/project/_stats.html", partial=partial)
+    # Stats for about infos in table partial = True or full about page partial = False
+    from appli.gui.project.projectsettings import get_target_prj
+
+    print(prjid)
+    prj = get_target_prj(prjid)
+    return render_template("v2/project/_stats.html", partial=True, prj=prj)
+
     with ApiClient(ProjectsApi, request) as api:
         stats: ProjectSetColumnStatsModel = api.project_set_get_column_stats(
             ids=prjid,
@@ -23,9 +27,3 @@ def prj_stats(prjid: int, partial: bool, params: dict) -> str:
             categories="403,2",
         )
     total = stats.total
-    for col, count, variance in zip(stats.columns, stats.counts, stats.variances):
-        prfx, name = col.split(".", 1)
-        critlist[name][1] = round(
-            100 * (1 - count / stats.total)
-        )  # % Missing in source projects
-        critlist[name][2] = " " if variance is None else ("Y" if variance != 0 else "N")
