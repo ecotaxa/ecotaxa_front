43bf623057849b2da4083c7163db6508af0c8675
bea777515 <beatrice.caraveo@imev-mer.fr>
2024-02-05 14:52:02 +0100

    fix - page unload

--- dev_gui/src/index.js
+++ dev_gui/src/index.js
@@ -22,21 +22,21 @@ import {
       item.classList.toggle('over');
     });
   });*/
-//window.addEventListener('load', async () => {
-// enhance ui activate components  js js-componentname
-const jsComponents = new JsComponents();
-jsComponents.applyTo();
-/*** activate data-action  **/
-const activItems = new ActivItems(document);
-/* alert boxes */
-const alertBox = new AlertBox();
-/* fetch request -modal - contextual help */
-const activRequest = new ActivRequest();
-activRequest.applyTo(document);
+window.addEventListener('load', async () => {
+  // enhance ui activate components  js js-componentname
+  const jsComponents = new JsComponents();
+  jsComponents.applyTo();
+  /*** activate data-action  **/
+  const activItems = new ActivItems(document);
+  /* alert boxes */
+  const alertBox = new AlertBox();
+  /* fetch request -modal - contextual help */
+  const activRequest = new ActivRequest();
+  activRequest.applyTo(document);
 
-//});
-// 'unload' but 'pagehide'
-window.addEventListener('pagehide', (e) => {
+});
+// 'unload' - reload js when history back
+window.addEventListener('unload', (e) => {
   //  ecotaxaStorage.clear();
 });
 /*
