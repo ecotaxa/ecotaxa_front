937d3cba928776985a7ac0363c3d269c21f08864
bea777515 <beatrice.caraveo@imev-mer.fr>
2024-02-09 17:35:22 +0100

    dist --

--- appli/static/gui/dist/856.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 856.js.LICENSE.txt */
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[856],{4856:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var n=a(2817);function s(e){return{taxa:(t,a,s,r={})=>{Array.isArray(t)||(r.childNodes=[]);let d=[];return t.forEach((e=>{d.push(e[1])})),r.childnodes=[{nodename:"DIV",attributes:{class:n.iv.component.table.tip,"data-num":d.length},childnodes:[e.setTextNode(d.join(", "))]}],r}}}}}]);
