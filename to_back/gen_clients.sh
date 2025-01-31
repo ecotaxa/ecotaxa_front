ba3a1dd3d358b051847e88b3a06eee732dc6b22c
grololo06 <laurent.salinas@laposte.net>
2025-01-31 07:16:45 +0100

    chore: reconstitue version in prod'

--- to_back/gen_clients.sh
+++ to_back/gen_clients.sh
@@ -10,6 +10,7 @@ if test -f "openapi.json"; then
   # A bit disturbing, but it's the same file, openapi.json in current directory is in /client as seen from docker
   SRC="/client/to_back/openapi.json"
 fi
+SRC="https://raw.githubusercontent.com/ecotaxa/ecotaxa_back/master/openapi.json"
 # openapi generator removed any python typings or just auto-completion hints in 5.1.1,
 # so the main point of generators is a bit lost to me.
 docker run --rm --network="host" -v ${PWD}/..:/client -u $(id -u ${USER}):$(id -g ${USER}) \
