fc3bb769604003d8f407373fadbeb6894f03ec2b
grololo06 <laurent.salinas@laposte.net>
2025-12-16 12:49:31 +0100

    CI(https://app.clickup.com/t/86c70hpjw): la GH action reflète les scripts manuels

---
+++ docker/build_clean_src.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Prepare a clean source tree in ./py/ for building the production Docker image.
+set -euo pipefail
+# Generate list of untracked files to exclude from rsync
+(cd .. && git status --porcelain --ignored | grep -e "^??" -e "^!!" | sed -e "s/.. //g" > docker/not_in_git.lst || true)
+# Sync repository (parent dir) into docker/py, excluding unwanted and untracked files
+rsync -avr \
+  --delete \
+  --exclude=docker \
+  --exclude-from=not_to_copy.lst \
+  --exclude-from=not_in_git.lst \
+  --delete-excluded \
+  .. py/
+echo "Clean build source prepared in docker/py"
--- docker/build_prod.sh
+++ docker/build_prod.sh
@@ -4,11 +4,8 @@ VERSION=v2.7.9.3-beta
 #NO_CACHE=--no-cache
 # Preliminary, log using ecotaxa docker account
 #docker login -u ecotaxa
-# Copy all and only source files
-(cd .. && git status --porcelain | grep "^??" | sed -e "s/?? //g" > docker/not_in_git.lst)
-rsync -avr --delete --exclude-from=not_to_copy.lst --exclude-from=not_in_git.lst --delete-excluded .. py/
-mkdir -p py/docker/prod_image
-rsync -avr prod_image/*.sh prod_image/*.ini py/docker/prod_image/
+# Prepare clean source tree for build context
+bash build_clean_src.sh
 # Build
 docker build $NO_CACHE -t ecotaxa/ecotaxa_front -f prod_image/Dockerfile .
 # Push to docker hub
