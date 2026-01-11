9d6125f27577dbfb40125d3fdf34c6d8976eb63c
grololo06 <laurent.salinas@laposte.net>
2026-01-11 14:29:33 +0100

    dev(WoRMS): finally can create one thru UI

--- to_back/ecotaxa_cli_py/api/taxonomy_tree_api.py
+++ to_back/ecotaxa_cli_py/api/taxonomy_tree_api.py
@@ -190,7 +190,7 @@ class TaxonomyTreeApi(object):
     def add_worms_taxon(self, add_worms_taxon_model, **kwargs):  # noqa: E501
         """Add Worms Taxon  # noqa: E501
 
-        Add worms taxon with or without lineage information  # noqa: E501
+        Add worms taxon by its aphia_id  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.add_worms_taxon(add_worms_taxon_model, async_req=True)
@@ -205,7 +205,7 @@ class TaxonomyTreeApi(object):
                                  number provided, it will be total request
                                  timeout. It can also be a pair (tuple) of
                                  (connection, read) timeouts.
-        :return: None
+        :return: object
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -215,7 +215,7 @@ class TaxonomyTreeApi(object):
     def add_worms_taxon_with_http_info(self, add_worms_taxon_model, **kwargs):  # noqa: E501
         """Add Worms Taxon  # noqa: E501
 
-        Add worms taxon with or without lineage information  # noqa: E501
+        Add worms taxon by its aphia_id  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.add_worms_taxon_with_http_info(add_worms_taxon_model, async_req=True)
@@ -232,7 +232,7 @@ class TaxonomyTreeApi(object):
                                  number provided, it will be total request
                                  timeout. It can also be a pair (tuple) of
                                  (connection, read) timeouts.
-        :return: None
+        :return: tuple(object, status_code(int), headers(HTTPHeaderDict))
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -297,7 +297,7 @@ class TaxonomyTreeApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type=None,  # noqa: E501
+            response_type='object',  # noqa: E501
             auth_settings=auth_settings,
             async_req=local_var_params.get('async_req'),
             _return_http_data_only=local_var_params.get('_return_http_data_only'),  # noqa: E501
