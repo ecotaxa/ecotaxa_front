a4d3f00c444445309a66b33bcb0e67c8c600e1e6
grololo06 <laurent.salinas@laposte.net>
2026-03-29 12:47:29 +0200

    dev:TUS protocol - restore previous API on (unused) file upload

--- appli/gui/files/tools.py
+++ appli/gui/files/tools.py
@@ -45,23 +45,30 @@ def move_dir_file(source_path: str, dest_path: str) -> str:
 
 
 def upload_file():
-    from tusclient import client
+    # import json
+    import requests
 
     dirpath = gvp("path")
     uploaded: FileStorage = request.files.get("file")
-
+    # reqheaders = json.loads(json.dumps({k: v for k, v in request.headers.items()}))
     # Relay the file to back-end
     with ApiClient(MyFilesApi, request) as api:
-        url = api.api_client.configuration.host + "/big_files/upload"
+        # Call using requests, as the generated openapi wrapper only reads the full file in memory.
+        url = api.api_client.configuration.host + file_service
         token = api.api_client.configuration.access_token
-        headers = {"Authorization": "Bearer " + token}
-
-        my_client = client.TusClient(url, headers=headers)
-        uploader = my_client.uploader(
-            file_stream=uploaded.stream,
-            metadata={"filename": uploaded.filename, "path": dirpath},
-            chunk_size=10_000_000,
+        # reqheaders["Authorization"] = "Bearer " + token
+        headers = {
+            "Authorization": "Bearer " + token,
+            # "Content-Range": reqheaders["Content-Range"],
+            # "Transfer-Encoding": reqheaders["Transfer-Encoding"],
+        }
+        # headers = reqheaders
+        # 'requests' lib sends fine the name to back-end
+        uploaded.name = uploaded.filename
+        rsp = requests.post(
+            url,
+            data={"path": dirpath},
+            files={"file": uploaded},
+            headers=headers,
         )
-        uploader.upload()
-    # Tus doesn't return the same thing as the original POST
-    return {}, None
+    return rsp.json()
