c7df2a8330ad0222fdb152a88a7a7b6a69590e0a
bea777515 <beatrice.716@gmail.com>
2026-04-27 17:35:14 +0200

    mod: formulae

--- dev_gui/src/modules/js-dragdrop.js
+++ dev_gui/src/modules/js-dragdrop.js
@@ -8,10 +8,11 @@ export function JsDragDrop(item,options) {
   };
   Object.freeze(options);
   let currentcontainer=null, current={id:null, type:null};
+  let caretpos=null;
   const movehandlers=[{
       name: 'dragstart',
       func: (e) => {
-        handleDragStart(e);
+        handleDragStart(e,'copy');
       }
     }, {
       name: 'dragend',
@@ -38,15 +39,27 @@ export function JsDragDrop(item,options) {
         func: (e)=> {
           handleAction(e,'blur');
         }},
-     {name:'keypress',func: (e) => {
-          handleAction(e,'keypress');
+     { name:'keypress',func: (e) => {
+         // handleAction(e,'keypress');
+          handleSelection(e);
+         }},
+     { name:'mouseup',func: (e) => {
+          handleSelection(e);
+
+         }},
+     { name:'keyup',func: (e) => {
+          handleSelection(e);
+
+         }},
+     { name:'selectionchange',func: (e) => {
+          handleSelection(e);
 
          }
     }];
 
-  function handleDragStart(e) {
+  function handleDragStart(e,effect) {
     e.stopImmediatePropagation();
-    e.dataTransfer.effectAllowed = 'copy';
+    e.dataTransfer.effectAllowed = effect;
     current.id=e.target.id;
     current.type=e.target.textContent.split('.')[0];
     e.dataTransfer.setData("text/plain",current.id);
@@ -60,41 +73,72 @@ export function JsDragDrop(item,options) {
 
   function handleDragOver(e) {
     const accept =(e.target.dataset.accept)?e.target.dataset.accept:null;
-    if (e.target.hasOwnProperty('contentEditable') && accept.split(',').includes(current.type))  e.preventDefault();
-    else return false;
+    e.preventDefault();
+    if (accept!==null && e.target.isContentEditable && accept.split(',').includes(current.type))  {e.target.focus();return true;}
+    e.target.blur();return false;
   }
   function handleDragEnd(e) {
     e.preventDefault();
       return;
-    //document.getElementById(currentdrag).classList.remove(options.css.dragging);
   }
+  function getSelectionNodeInfo(e) {
+      const selection =window.window.getSelection();
+    const range = document.createRange();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    const x = e.pageX;
+    const y= e.pageY;
+    const elem = document.elementFromPoint(x,y );
+    let startnode = (elem.childNodes.length>0?elem.childNodes[0]:elem);
+    let startcharindex = -1;
+    let rect;
+    do {
+        startcharindex++;
+        range.setStart(startnode, startcharindex);
+        range.setEnd(startnode, startcharindex+1);
+        rect = range.getBoundingClientRect();
+    } while (rect.left<x && startcharindex<startnode.length-1);
 
+    return {node:startnode, offsetInsideNode:startcharindex};
+}
   function handleDrop(e) {
     /***/
-      if (e.target.disabled) return false;
+    if (currentcontainer!==e.target) {e.target.disabled=true;console.log('dropfalsefalse') ;return; }
     const el = document.getElementById(e.dataTransfer.getData("text/plain"));
      if (el===null) return;
-    const clone=el.cloneNode(true);
+     let clone=null;
+     if (!el.classList.contains(options.css.dropped)) {
+     clone=el.cloneNode(true);
     el.dataset.num=(el.dataset.num)?parseInt(el.dataset.num)+1:1;
     clone.id=clone.id+'_'+el.dataset.num;
-    clone.contenteditable=false;
+    clone.contentEditable=false;
     clone.classList.add(options.css.dropped);
-    clone.addEventListener('keydown',(e) => {console.log('ecode',e)
+    // clone.draggable=false;
+     clone.addEventListener('dblclick',(e) => {console.log('ecode',e)
        e.preventDefault();
-     if ([8,46].includes(e.code)) {const origin=clone.id.split('_');const num=parseInt(origin[1])-1;document.getElementById(origin[0]).dataset.num=num;clone.remove();}});
-      pasteHtmlAtCaret(clone,e.target);
-      console.log('e.targetvalue', e.target.textContent)
+         e.stopImmediatePropagation();
+         clone.remove();
+         });
+        clone.addEventListener('dragstart', (e) => {handleDragStart(e,'move');});
+     } else clone=el;
+
+
+       pasteElementAtCaret(clone,e);
   }
   function handleAction(e, action) {
       let no=false;
       switch(action) {
           case 'focus':
-          case 'keypress':
               currentcontainer=e.target;
+          break;
+          case 'keypress':
+             currentcontainer=e.target;
+             caretpos=getCaretPos(e.target);
           break;
           case 'blur':
               no=true;
               currentcontainer=null;
+              caretpos=null;
           break;
       }
       const accept =(e.target.dataset.accept)?e.target.dataset.accept:null;
@@ -105,36 +149,47 @@ export function JsDragDrop(item,options) {
       });
 
   }
-  function setCursorAt(container) {
-    const selection = window.getSelection();
-
-      console.log('cont', container)
-     if(container !==currentcontainer) { console.log('setcursorat')
-       const range = document.createRange();
-       selection.removeAllRanges();
-        range.selectNodeContents(container);
-        range.collapse(true);
-        selection.addRange(range);
-        currentcontainer = container;     }
-    container.focus();
+    function handleSelection(e) {
+      currentcontainer=e.target;
+      let selection = window.getSelection();
+      const startpos=window.getSelection().anchorOffset;
+      const endpos = window.getSelection().focusOffset;
+      caretpos= {start:startpos,end:endpos};
   }
-function pasteHtmlAtCaret(tag,container) {
-    setCursorAt(container);
+
+  function getCaretPos() {
     const selection = window.getSelection();
-    if (selection.getRangeAt && selection.rangeCount) {
-        let range = selection.getRangeAt(0);
-        range.deleteContents();
-        range.insertNode(tag);
-        range.setStartAfter(tag);
-        range.collapse(true);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-    }
+    const range = selection.getRangeAt(0);
+    const startpos = range.startOffset;
+    const endpos = range.endOffset;
+    return {start:startpos,end:endpos};
+}
+
+  function pasteElementAtCaret(tag,e) {
+      const container=e.target;
+     if (currentcontainer!==container) return;
+     if (caretpos===null || caretpos.start===caretpos.end) {
+         const nodeinfo=getSelectionNodeInfo(e);
+        caretpos=getCaretPos(container);
+     } else if(caretpos.end>caretpos.start) selection.extend(caretpos.end-caretpos.start);
+     const selection = window.getSelection();
+     const range =selection.getRangeAt(0);
+     range.setStart(selection.focusNode,caretpos.start);
+     range.setEnd(selection.focusNode,caretpos.end);
+     if (caretpos.end > caretpos.start) range.deleteContents();
+     range.insertNode(tag);
+     range.setStartAfter(tag);
+     range.collapse(true);
+     selection.removeAllRanges();
+     selection.addRange(range);
+      }
+
      item.querySelectorAll('[draggable]').forEach((tag) =>{
     movehandlers.forEach(listener=> { tag.addEventListener(listener.name,listener.func)});
     })
-    item.querySelectorAll('[data-accept]').forEach( (droparea) => { droparea.contentEditable=true;drophandlers.forEach(listener => {droparea.addEventListener(listener.name,listener.func);})});
+    item.querySelectorAll('[data-accept]').forEach( (droparea) => {
+        droparea.contentEditable=true;
+        drophandlers.forEach(listener => {droparea.addEventListener(listener.name,listener.func);})});
    if(item.dataset.hasOwnProperty('formhandler')) {
        const form=item.closest('form');
        if (form.formsubmit) form.formsubmit.addHandler('submit',() =>{
